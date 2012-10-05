6976e226f8f654188ae7835d17ef50e1a2cd3e16
Michael Petychakis <mpetyx@gmail.com>
2012-10-05 17:34:40 +0300

    change on procfile

--- openi/openi/wsgi.py
+++ openi/openi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for moodeetHeroku project.
+WSGI config for openiHeroku project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
