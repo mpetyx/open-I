7ed778c75c2c043c47a5318b5649242542359822
Michael Petychakis <mpetyx@gmail.com>
2013-11-14 14:48:52 +0200

    update database

--- OPENiapp/OPENiapp/Objects/Photo/twitter.py
+++ OPENiapp/OPENiapp/Objects/Photo/twitter.py
@@ -9,10 +9,10 @@ from allauth.socialaccount.models import SocialToken, SocialApp
 # from OPENiapp.allauth.socialaccount.models import SocialToken, SocialApp
 
 
-APP_KEY = 'Uifi6oR2hXaDaUGtTT61hw'
-APP_SECRET = 'UyCcdRcYO4Ls084dGZ5FaQVG1Il3FL1EnQI7doMs'
-OAUTH_TOKEN = '95257276-erB33trzmvpc27G0hQhMT9j8QNfWAJx8mML3uVQKU'
-OAUTH_TOKEN_SECRET = 'z0HTviJNm7iRXnTDbKNI9iJ5PSrRW7FnXgjh4tCVGuAJ1'
+# APP_KEY = 'Uifi6oR2hXaDaUGtTT61hw'
+# APP_SECRET = 'UyCcdRcYO4Ls084dGZ5FaQVG1Il3FL1EnQI7doMs'
+# OAUTH_TOKEN = '95257276-erB33trzmvpc27G0hQhMT9j8QNfWAJx8mML3uVQKU'
+# OAUTH_TOKEN_SECRET = 'z0HTviJNm7iRXnTDbKNI9iJ5PSrRW7FnXgjh4tCVGuAJ1'
 
 # twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 # print json.dumps(twitter.verify_credentials(), sort_keys=True, indent=4)
